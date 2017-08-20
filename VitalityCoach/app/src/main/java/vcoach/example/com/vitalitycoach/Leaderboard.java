c447e9224fd5600a8e580e4c1f58c573b1d9d2a3
ashwini2 <ashwinik@discovery.co.za>
2017-08-20 20:35:07 +0200

    integration with leaderboard

---
+++ VitalityCoach/app/src/main/java/vcoach/example/com/vitalitycoach/Leaderboard.java
@@ -0,0 +1,25 @@
+package vcoach.example.com.vitalitycoach;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ASHWINI2 on 16/07/2017.
+ */
+
+public class Leaderboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.leaderboard);
+    }
+
+}
